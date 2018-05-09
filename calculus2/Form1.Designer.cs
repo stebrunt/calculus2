43fee8594f0aaf50853875382fad081b7b2bc0e7
RC0026188 <RC0026188@CRO-ICF07-08>
2018-05-09 11:30:19 +0100

    New graphs code

--- calculus2/Form1.Designer.cs
+++ calculus2/Form1.Designer.cs
@@ -29,11 +29,137 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.graphsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.voltTimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dVdtTimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.graphsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(768, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveGraphToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(343, 50);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // graphsToolStripMenuItem
+            // 
+            this.graphsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.voltTimeToolStripMenuItem,
+            this.dVdtTimeToolStripMenuItem});
+            this.graphsToolStripMenuItem.Name = "graphsToolStripMenuItem";
+            this.graphsToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.graphsToolStripMenuItem.Text = "graphs";
+            // 
+            // voltTimeToolStripMenuItem
+            // 
+            this.voltTimeToolStripMenuItem.Name = "voltTimeToolStripMenuItem";
+            this.voltTimeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.voltTimeToolStripMenuItem.Text = "volt + time";
+            this.voltTimeToolStripMenuItem.Click += new System.EventHandler(this.voltTimeToolStripMenuItem_Click);
+            // 
+            // dVdtTimeToolStripMenuItem
+            // 
+            this.dVdtTimeToolStripMenuItem.Name = "dVdtTimeToolStripMenuItem";
+            this.dVdtTimeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dVdtTimeToolStripMenuItem.Text = "dV/dt - time";
+            this.dVdtTimeToolStripMenuItem.Click += new System.EventHandler(this.dVdtTimeToolStripMenuItem_Click);
+            // 
+            // saveGraphToolStripMenuItem
+            // 
+            this.saveGraphToolStripMenuItem.Name = "saveGraphToolStripMenuItem";
+            this.saveGraphToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveGraphToolStripMenuItem.Text = "save graph";
+            this.saveGraphToolStripMenuItem.Click += new System.EventHandler(this.saveGraphToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 389);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ToolStripMenuItem graphsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem voltTimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dVdtTimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveGraphToolStripMenuItem;
     }
 }
 
--- calculus2/Form1.cs
+++ calculus2/Form1.cs
@@ -3,18 +3,176 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace calculus2
 {
     public partial class Form1 : Form
     {
+        class row
+        {
+            public double time;
+            public double voltage;
+            public double current;
+            public double voltageDerivative;
+            public double charge;
+        }
+
+        List<row> table = new List<row>();
+
+        void tableSort()
+        {
+            table = table.OrderBy(x => x.time).ToList();
+        }
+
+
+        void derivative()
+        {
+            for (int i = 1; i < table.Count; i++)
+            {
+                double dV = table[i].voltage - table[i - 1].voltage;
+                double dt = table[i].time - table[i - 1].time;
+                table[i].voltageDerivative = dV / dt;
+            }
+        }
+
+
+
         public Form1()
         {
             InitializeComponent();
+
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+
+
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "CSV Files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+
+
+                        {
+                            string line = sr.ReadLine();
+                            while (!sr.EndOfStream)
+                            {
+                                table.Add(new row());
+                                string[] l = sr.ReadLine().Split(',');
+                                table.Last().time = double.Parse(l[0]);
+                                table.Last().voltage = double.Parse(l[1]);
+                                table.Last().current = double.Parse(l[2]);
+                            }
+  
+                        }
+                    }
+
+                }
+
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " Failed to open");
+
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the requiered format");
+                }
+
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
+                }
+
+
+
+
+
+
+            }
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void voltTimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            Series series1 = new Series
+            {
+                Name = "Points",
+                Color = Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Spline,
+                BorderWidth = 2
+
+            };
+
+            chart1.Series.Add(series1);
+            for (int i = 0; i < table.Count; i++)
+            {
+                series1.Points.AddXY(table[i].time, table[i].voltage);
+            }
+
+            chart1.ChartAreas[0].AxisX.Title = "time / s";
+            chart1.ChartAreas[0].AxisY.Title = "voltage / V";
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+        private void dVdtTimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            derivative();
+
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            Series series1 = new Series
+            {
+                Name = "Points",
+                Color = Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Spline,
+                BorderWidth = 2
+
+            };
+
+            chart1.Series.Add(series1);
+            for (int i = 0; i < table.Count; i++)
+            {
+                series1.Points.AddXY(table[i].time, table[i].voltageDerivative);
+            }
+
+            chart1.ChartAreas[0].AxisX.Title = "time / s";
+            chart1.ChartAreas[0].AxisY.Title = "voltage / V";
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+        private void saveGraphToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chart1.SaveImage(, ChartImageFormat.Png);
         }
     }
 }
